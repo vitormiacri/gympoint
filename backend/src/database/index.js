28469cd0ea66c774b092a4dacf7d4e3a9e5621ec
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-08 17:43:46 -0300

    Configuração do postgres

---
+++ backend/src/database/index.js
@@ -0,0 +1,34 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import User from '../app/Models/User';
+import Student from '../app/Models/Student';
+
+import databaseConfig from '../config/database';
+
+const models = [User, Student];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+}
+
+export default new Database();

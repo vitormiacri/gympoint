a5f902206b281fa584fcbc0402769839930bb603
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-09 19:07:19 -0300

    Routes file

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
-
 import validateSessionStore from './app/validators/sessions/SessionStore';
+
+import StudentController from './app/controllers/StudentController';
+import validateStudentStore from './app/validators/student/StudentCreateAndUpdate';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', validateSessionStore, SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/students', validateStudentStore, StudentController.store);
 
 export default routes;

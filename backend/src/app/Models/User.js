d7d6cb675bd7e32e5302dc93efccfdff48a00297
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-12 12:08:51 -0300

    Remove and refactor Users

--- backend/src/app/Models/User.js
+++ backend/src/app/Models/User.js
@@ -14,12 +14,6 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', async user => {
-      if (user.password) {
-        user.password_hash = await bcrypt.hash(user.password, 8);
-      }
-    });
-
     return this;
   }
 

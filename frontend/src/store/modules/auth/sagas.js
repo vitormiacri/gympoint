b361ae8f9053e1d0587ff7a74eef107d34a8b1ae
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-16 19:11:54 -0300

    Add Redux/Redux Saga config

---
+++ frontend/src/store/modules/auth/sagas.js
@@ -0,0 +1,51 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signFailure, signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { user, token } = response.body;
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    toast.success('Login efetuado com sucesso!');
+    history.push('/alunos');
+  } catch (err) {
+    console.tron.log(err.response);
+    toast.error(`Erro na Autenticaç: ${err.response}`);
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);

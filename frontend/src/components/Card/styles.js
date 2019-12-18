0de7621dd4d9a91a5e2ce3f0b125e5bc9c0177a5
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-18 07:48:54 -0300

    Card Component

---
+++ frontend/src/components/Card/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+
+  padding: 1.5rem;
+  margin-top: 1.5rem;
+`;

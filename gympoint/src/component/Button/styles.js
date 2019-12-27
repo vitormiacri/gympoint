bbd5c5eb706040441c713199f011565394b489b4
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-27 13:52:02 -0300

    Button component

---
+++ gympoint/src/component/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 45px;
+  background: #ee4e62;
+
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+`;

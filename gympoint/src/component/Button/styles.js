701c86df69b09d06301a07b7b7f28c0bca4fe4e5
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-30 11:30:40 -0300

    Fix button component

--- gympoint/src/component/Button/styles.js
+++ gympoint/src/component/Button/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+export const Container = styled(RectButton)`
   height: 45px;
   background: #ee4e62;
 

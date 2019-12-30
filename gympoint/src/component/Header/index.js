12e81461f4a6dbde72ce7b8a54322a159809f1cf
Vitor Morelli Miacri <vitao.miacri@gmail.com>
2019-12-30 11:29:22 -0300

    Add header component

---
+++ gympoint/src/component/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container, LogoTitle } from './styles';
+import logo from '~/assets/logo.png';
+
+export default function Header() {
+  return (
+    <Container>
+      <Image source={logo} style={{ width: 36, height: 14 }} />
+      <LogoTitle>GYMPOINT</LogoTitle>
+    </Container>
+  );
+}
---
+++ gympoint/src/component/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 45px;
+
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid #ddd;
+`;
+
+export const LogoTitle = styled.Text`
+  color: #ee4e62;
+  font-size: 14px;
+  font-weight: bold;
+  margin-left: 10px;
+`;
